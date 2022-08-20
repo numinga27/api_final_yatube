1889e237786e99e72a47e0b90fdd2c0f56226851
Андрей Крылов <numinga27@yandex.ru>
2022-08-20 23:23:12 +0400

    22

--- yatube_api/api/pangination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from .constants import MAX_PAGE
-
-
-class PostsPagination(PageNumberPagination):
-    page_size = MAX_PAGE
-     
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
 from rest_framework.pagination import LimitOffsetPagination
